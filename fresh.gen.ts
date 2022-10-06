b20a07ea815d4e9f820ab282db3091df1dd3cb98
Adam Christian <adam.christian@gmail.com>
2022-10-06 15:02:28 -0700

    What happens of _app goes away for the prod deploy

--- fresh.gen.ts
+++ fresh.gen.ts
@@ -3,17 +3,15 @@
 // This file is automatically updated during development when running `dev.ts`.
 
 import config from "./deno.json" assert { type: "json" };
-import * as $0 from "./routes/_app.tsx";
-import * as $1 from "./routes/blog.tsx";
-import * as $2 from "./routes/dev.tsx";
-import * as $3 from "./routes/index.tsx";
+import * as $0 from "./routes/blog.tsx";
+import * as $1 from "./routes/dev.tsx";
+import * as $2 from "./routes/index.tsx";
 
 const manifest = {
   routes: {
-    "./routes/_app.tsx": $0,
-    "./routes/blog.tsx": $1,
-    "./routes/dev.tsx": $2,
-    "./routes/index.tsx": $3,
+    "./routes/blog.tsx": $0,
+    "./routes/dev.tsx": $1,
+    "./routes/index.tsx": $2,
   },
   islands: {},
   baseUrl: import.meta.url,
