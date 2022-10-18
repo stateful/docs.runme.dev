c22440f614a34bf89e4de812fefc755941b9ac36
Christian Bromann <git@bromann.dev>
2022-10-18 15:37:58 -0700

    Minor tweaks

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -21,11 +21,12 @@ export function Hero() {
           </span>
         </h3>
         <div class="flex flex-col items-center justify-center py-12 space-y-4">
-          <div class="bg-[#5B3ADF] py-[20px] rounded-[60px] text-base w-[280px] h-[40px] flex items-center justify-center font-semibold">
-            <a href="https://marketplace.visualstudio.com/items?itemName=stateful.runme">
-              Install the extension
-            </a>
-          </div>
+          <a
+            href="https://marketplace.visualstudio.com/items?itemName=stateful.runme"
+            class="bg-[#5B3ADF] py-[20px] rounded-[60px] text-base w-[280px] h-[40px] leading-[0px] font-semibold"
+          >
+            Install the extension
+          </a>
 
           <div class="">
             or search{" "}
