50232432e487922afa5037ccb3c6674f72539c15
Ruben Restrepo <ruben@stateful.com>
2023-03-08 14:57:48 -0500

    Decode properly fileToOpen

--- api/runme.js
+++ api/runme.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
     const { repository, file } = req.query
     if (!repository) return res.status(500).send('Expecting a repository to open')
-    res.redirect(308, `vscode://stateful.runme?command=setup&repository=${decodeURIComponent(repository)}&fileToOpen=${file || 'README.md'}`)
+    res.redirect(308, `vscode://stateful.runme?command=setup&repository=${decodeURIComponent(repository)}&fileToOpen=${decodeURIComponent(file) || 'README.md'}`)
   }
