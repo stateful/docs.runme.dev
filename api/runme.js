57b346f58d72473dc1b1186ceedc666d707f8c89
Ruben Restrepo <ruben@stateful.com>
2023-03-29 18:12:18 -0500

    Fix sending undefined as string when missing query fileToOpen

--- api/runme.js
+++ api/runme.js
@@ -1,5 +1,5 @@
 export default function handler(req, res) {
-    const { repository, fileToOpen } = req.query
+    const { repository, fileToOpen = 'README.md' } = req.query
     if (!repository) return res.status(500).send('Expecting a repository to open')
-    res.redirect(308, `vscode://stateful.runme?command=setup&repository=${decodeURIComponent(repository)}&fileToOpen=${decodeURIComponent(fileToOpen) || 'README.md'}`)
+    res.redirect(308, `vscode://stateful.runme?command=setup&repository=${decodeURIComponent(repository)}&fileToOpen=${decodeURIComponent(fileToOpen)}`)
   }
