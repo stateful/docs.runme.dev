b43751f428783e48b3ba16ea4771144263bf1e3b
Christian Bromann <git@bromann.dev>
2023-04-12 15:19:03 -0700

    prefer every two weeks

--- src/pages/events.tsx
+++ src/pages/events.tsx
@@ -6,7 +6,7 @@ import Footer from '@site/src/components/Footer'
 import { EventsHero } from '@site/src/components/Hero'
 
 export default function Home() {
-  const bgImage = '/svg/community-events.svg'
+  const bgImage = 'https://runme.dev/svg/community-events.svg'
   const title = 'Runme Community Events'
   const description = 'Join us in our bi-weekly Runme community meetings and let us have a chat on Runme and the DevX space as a whole'
 
@@ -16,7 +16,7 @@ export default function Home() {
       description={description}
       wrapperClassName="main events"
     >
-      <Meta title={title} description={description} imageUrl={bgImage} />
+      <Meta title={title} description={description} imageUrl={bgImage} previewImages={[bgImage]} />
       <EventsHero />
       <iframe
         src="https://notioniframe.com/notion/wfwbs2chcc"
