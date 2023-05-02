e63ddd7f7709962db7217a7162c8181546e6c047
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2023-05-02 09:32:26 -0400

    Change unfurl

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,8 @@
 export const title = "Runme: Run your Readme.md";
-export const imageUrl = "https://runme.dev/img/intro.gif";
+export const imageUrl = "https://runme.dev/img/unfurl.gif";
 export const description =
   "Runme is a VS Code extension that transparently opens markdown files as runnable VS Code notebooks so that repo maintainers can annotate code blocks to enhance the interactive runbook-like experience.";
 export const previewImages = [
-  "https://runme.dev/img/intro.gif",
+  "https://runme.dev/img/unfurl.gif",
   "https://runme.dev/img/sidebyside.png",
 ];
