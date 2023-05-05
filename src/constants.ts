6bc7dbe11eca640836aae2f0c446cc073df0b835
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2023-05-05 14:08:12 -0400

    Add period

--- src/constants.ts
+++ src/constants.ts
@@ -1,7 +1,7 @@
 export const title = "Runme: Run your Readme.md";
 export const imageUrl = "https://runme.dev/img/unfurl.gif";
 export const description =
-  "Runme is a next-gen runner that uses markdown to express workflows for your apps & services to make developer experience reliable and portable. Runme comes with a command line interface and a first-class integration into VS Code";
+  "Runme is a next-gen runner that uses markdown to express workflows for your apps & services to make developer experience reliable and portable. Runme comes with a command line interface and a first-class integration into VS Code.";
 export const previewImages = [
   "https://runme.dev/img/unfurl.gif",
   "https://runme.dev/img/sidebyside.png",
