e733682f4e536e4040dc45fd8fdde11cca9d2521
Sebastian Tiedtke <sebastiantiedtke@gmail.com>
2023-05-07 13:01:59 -0400

    Match title

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const title = "Runme: Run your Readme.md";
+export const title = "Runme: The Next-Gen Runner for dev workflows";
 export const imageUrl = "https://runme.dev/img/unfurl.gif";
 export const description =
   "Runme is a next-gen runner that uses markdown to express workflows for your apps & services to make developer experience reliable and portable. Runme comes with a command line interface and a first-class integration into VS Code.";
