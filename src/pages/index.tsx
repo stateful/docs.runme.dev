a4b584a736b5d23143194e6f10a6f4fefdfe2d1c
Adam Christian <adam.christian@gmail.com>
2023-05-08 10:00:11 -0700

    Some new icons

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import Layout from '@theme/Layout';
+import React from "react";
+import Layout from "@theme/Layout";
 
-import Meta from '@site/src/components/Meta'
-import Footer from '@site/src/components/Footer'
-import { Hero } from '@site/src/components/Hero'
-import { Benefits, HowItWorks } from '@site/src/components/Sections';
+import Meta from "@site/src/components/Meta";
+import Footer from "@site/src/components/Footer";
+import { Hero } from "@site/src/components/Hero";
+import { Benefits } from "@site/src/components/Sections";
 
 export default function Home() {
   return (
@@ -13,11 +13,6 @@ export default function Home() {
       <div className="relative mb-24 overflow-x-hidden">
         <Hero />
         <Benefits />
-        {/* <HowItWorks /> */}
-        {/* <OpenSource /> */}
-        {/* <CLI /> */}
-        {/* <Documentation /> */}
-        {/* <FinalCta /> */}
       </div>
       <Footer />
     </Layout>
