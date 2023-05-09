8802c114eeaa4f5acc9625aebf4becf3ad704712
Adam Christian <adam.christian@gmail.com>
2023-05-09 10:42:06 -0700

    Maybe testimonials and bigger buttons helps?

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Layout from "@theme/Layout";
+import InstallCTA from "@site/src/components/InstallCta";
 
 import Meta from "@site/src/components/Meta";
 import Footer from "@site/src/components/Footer";
 import { Hero } from "@site/src/components/Hero";
 import { Benefits } from "@site/src/components/Sections";
+import Testimonials from "@site/src/components/Testimonials";
 
 export default function Home() {
   return (
@@ -13,6 +15,8 @@ export default function Home() {
       <div className="relative mb-24 overflow-x-hidden">
         <Hero />
         <Benefits />
+        <Testimonials />
+        <InstallCTA />
       </div>
       <Footer />
     </Layout>
