dd1252b4adae9fd63e8368415ac16f0ddb7fa1be
Adam Christian <adam.christian@gmail.com>
2023-05-15 17:24:40 -0700

    Add new twitter

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -32,6 +32,12 @@ export function QuickShell({ command }) {
   );
 }
 
+const TwitterIcon = () => (
+  <svg className="h-8" fill="currentColor" viewBox="0 0 24 24">
+    <path d="M8.29 20.251c7.547 0 11.675-6.253 11.675-11.675 0-.178 0-.355-.012-.53A8.348 8.348 0 0022 5.92a8.19 8.19 0 01-2.357.646 4.118 4.118 0 001.804-2.27 8.224 8.224 0 01-2.605.996 4.107 4.107 0 00-6.993 3.743 11.65 11.65 0 01-8.457-4.287 4.106 4.106 0 001.27 5.477A4.072 4.072 0 012.8 9.713v.052a4.105 4.105 0 003.292 4.022 4.095 4.095 0 01-1.853.07 4.108 4.108 0 003.834 2.85A8.233 8.233 0 012 18.407a11.616 11.616 0 006.29 1.84" />
+  </svg>
+);
+
 export function Hero() {
   return (
     <div className="relative px-4 pt-12 space-y-12 text-center text-white md:px-0 md:pt-24">
@@ -73,7 +79,6 @@ export function Hero() {
               Documentation
             </div>
           </a>
-
           <a
             href="https://github.com/stateful/runme"
             className="w-full sm:max-w-min"
@@ -82,6 +87,11 @@ export function Hero() {
               View on GitHub
             </div>
           </a>
+          <div>
+            <a href="https://twitter.com/runmedev">
+              <TwitterIcon />
+            </a>
+          </div>
         </div>
         <div className="flex flex-col items-center justify-center py-2 space-y-1 text-xs text-gray-300 md:space-x-2 md:flex-row md:space-y-0">
           <QuickShell command={"npx runme"} />
